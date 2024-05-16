a11ee564016bb435909fe8db455f1c9ad7b53b60
Inshad <inshadaliqbal786@gmail.com>
2024-05-16 22:51:07 +0530

    UI fixing

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -5,7 +5,6 @@ import 'package:personaltaskmanager/task_screen.dart';
 
 final _auth = FirebaseAuth.instance;
 
-
 class LoginScreen extends StatefulWidget {
   static String loginScreen = 'login_screen';
   const LoginScreen({super.key});
@@ -22,128 +21,263 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Center(
-          child: Text('Registration'),
-        ),
-      ),
       body: ModalProgressHUD(
         inAsyncCall: setSpinner!,
-        child: SafeArea(
-          child: Container(
-            padding: EdgeInsets.all(10),
+        child: CustomPaint(// Adjust size as needed
+          painter: DrawingBookPainter(),
+          child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Flexible(
-                  fit: FlexFit.loose,
-                  child: Hero(
-                    tag: 'logo',
-                    child: Icon(
-                      Icons.electric_bolt,
-                      size: 100,
-                      color: Colors.orangeAccent,
-                    ),
-                  ),
-                ),
                 Container(
+                  padding: EdgeInsets.all(10),
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      Container(
-                        child: TextField(
-                          keyboardType: TextInputType.emailAddress,
-                          onChanged: (value) {
-                            email = value;
-                          },
-                          textAlign: TextAlign.right,
-                          decoration: InputDecoration(
-                              border: UnderlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(50),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Center(
+                            child: Text(
+                              'Welcome Back',
+                              style: TextStyle(
+                                fontFamily: 'germenia',
+                                color: Colors.black,
+                                fontSize: 40,
+                                // fontWeight: FontWeight.w400
+                              ),
+                            ),
+                          ),
+                          Flexible(
+                            fit: FlexFit.loose,
+                            child: Hero(
+                              tag: 'logo',
+                              child: Icon(
+                                Icons.task_sharp,
+                                size: 50,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.symmetric(vertical: 10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(
+                                  10), // Adjust border radius as needed
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black, // Shadow color
+                                  offset: Offset(
+                                      5, 5), // No offset in X and Y direction
+                                  blurRadius: 0, // Adjust blur radius as needed
+                                  spreadRadius:
+                                  0, // Negative spread radius to prevent spreading on the right and bottom sides
                                 ),
+                              ],
+                            ),
+                            child: TextField(
+                              keyboardType: TextInputType.emailAddress,
+                              onChanged: (value) {
+                                email = value;
+                              },
+                              textAlign: TextAlign.right,
+                              decoration: InputDecoration(
+                                labelText: 'EMAIL',
+                                labelStyle: TextStyle(letterSpacing: 3),
+                                filled: true,
+                                fillColor:
+                                Colors.white, // Set background color to white
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      width: 1,
+                                      color: Colors
+                                          .black), // Set border width and color
+                                  borderRadius: BorderRadius.circular(
+                                      10), // Adjust border radius as needed
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      width: 1,
+                                      color: Colors
+                                          .black), // Set border width and color
+                                  borderRadius: BorderRadius.circular(
+                                      10), // Adjust border radius as needed
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      width: 1,
+                                      color: Colors
+                                          .black), // Set border width and color
+                                  borderRadius: BorderRadius.circular(
+                                      10), // Adjust border radius as needed
+                                ),
+                                // Adjust content padding as needed
+                                contentPadding:
+                                EdgeInsets.symmetric(horizontal: 10),
                               ),
-                              icon: Icon(
-                                Icons.person,
-                                size: 30,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.symmetric(vertical: 10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(
+                                  10), // Adjust border radius as needed
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black, // Shadow color
+                                  offset: Offset(
+                                      5, 5), // No offset in X and Y direction
+                                  blurRadius: 0, // Adjust blur radius as needed
+                                  spreadRadius:
+                                  0, // Negative spread radius to prevent spreading on the right and bottom sides
+                                ),
+                              ],
+                            ),
+                            child: TextField(
+                              keyboardType: TextInputType.emailAddress,
+                              onChanged: (value) {
+                                password = value;
+                              },
+                              textAlign: TextAlign.right,
+                              decoration: InputDecoration(
+                                labelText: 'PASSWORD',
+                                labelStyle: TextStyle(letterSpacing: 3),
+                                filled: true,
+                                fillColor:
+                                Colors.white, // Set background color to white
+                                border: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      width: 1,
+                                      color: Colors
+                                          .black), // Set border width and color
+                                  borderRadius: BorderRadius.circular(
+                                      10), // Adjust border radius as needed
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      width: 1,
+                                      color: Colors
+                                          .black), // Set border width and color
+                                  borderRadius: BorderRadius.circular(
+                                      10), // Adjust border radius as needed
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      width: 1,
+                                      color: Colors
+                                          .black), // Set border width and color
+                                  borderRadius: BorderRadius.circular(
+                                      10), // Adjust border radius as needed
+                                ),
+                                // Adjust content padding as needed
+                                contentPadding:
+                                EdgeInsets.symmetric(horizontal: 10),
                               ),
-                              iconColor: Colors.black12,
-                              labelText: 'USERNAME',
-                              labelStyle: TextStyle(letterSpacing: 3),
-                              filled: true,
-                              fillColor: Colors.cyan),
-                        ),
-                        padding: EdgeInsets.all(10),
+                            ),
+                          ),
+                        ],
                       ),
                       Container(
-                        padding: EdgeInsets.all(10),
-                        child: TextField(
-                          keyboardType: TextInputType.emailAddress,
-                          onChanged: (value) {
-                            password = value;
+                        margin: EdgeInsets.symmetric(vertical: 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(
+                              10), // Adjust border radius as needed
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black, // Shadow color
+                              offset:
+                              Offset(5, 5), // No offset in X and Y direction
+                              blurRadius: 0, // Adjust blur radius as needed
+                              spreadRadius:
+                              0, // Negative spread radius to prevent spreading on the right and bottom sides
+                            ),
+                          ],
+                        ),
+                        child: TextButton(
+                          style: ButtonStyle(
+                            elevation: MaterialStateProperty.all(8.0),
+                            backgroundColor: MaterialStateProperty.all(Colors
+                                .lightBlueAccent
+                                .shade400), // Set background color to white
+                            shape: MaterialStateProperty.all(
+                              // Apply shape with border radius
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                    10), // Adjust border radius as needed
+                                side: BorderSide(
+                                    width: 1,
+                                    color: Colors
+                                        .black), // Add border with width and color
+                              ),
+                            ),
+                            minimumSize: MaterialStateProperty.all(
+                                Size(300, 50)), // Set button size
+                          ),
+                          onPressed: () async {
+                            setState(() {
+                              setSpinner = true;
+                            });
+                            try {
+                              // await _auth.signInWithEmailAndPassword(email: email!, password: password!);
+                              Navigator.pushNamed(context, TaskScreen.taskScreen);
+                              setState(() {
+                                setSpinner = false;
+                              });
+                            } catch (e) {
+                              print(e);
+                            }
                           },
-                          textAlign: TextAlign.right,
-                          decoration: InputDecoration(
-                              border: UnderlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(50),
-                                ),
-                              ),
-                              icon: Icon(
-                                Icons.person,
-                                size: 30,
-                              ),
-                              iconColor: Colors.black12,
-                              labelText: 'PASSWORD',
-                              labelStyle: TextStyle(letterSpacing: 3),
-                              filled: true,
-                              fillColor: Colors.cyan),
+                          child: Text(
+                            'LOG IN',
+                            style: TextStyle(
+                                color: Colors.white), // Set text color to black
+                          ),
                         ),
                       )
                     ],
                   ),
-                ),
-                TextField(),
-                TextButton(
-                  style: ButtonStyle(
-                    elevation: MaterialStatePropertyAll(8.0),
-                    shadowColor: MaterialStatePropertyAll(Colors.black),
-                    backgroundColor:
-                    MaterialStatePropertyAll(Colors.blueAccent.shade200),
-                    fixedSize: MaterialStatePropertyAll(
-                      Size(300, 50),
-                    ),
-                    shape: MaterialStatePropertyAll(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                    ),
-                  ),
-                  onPressed: () async {
-                    setState(() {
-                      setSpinner = true;
-                    });
-                    try {
-                      await _auth.signInWithEmailAndPassword(
-                          email: email!, password: password!);
-                      Navigator.pushNamed(context, TaskScreen.taskScreen);
-                      setState(() {
-                        setSpinner = false;
-                      });
-                    } catch (e) {
-                      print(e);
-                    }
-                  },
-                  child: Text(
-                    'LOG IN',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
+                )
               ],
             ),
           ),
         ),
       ),
     );
+  }
+}
+
+class DrawingBookPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.black.withOpacity(0.1)
+      ..strokeWidth = 1.0
+
+    ;
+
+    final double step = 15.0; // Adjust the spacing between lines
+    final double maxX = size.width;
+    final double maxY = size.height;
+
+    // Drawing vertical lines
+    for (double i = 0; i <= maxX; i += step) {
+      canvas.drawLine(Offset(i, 0), Offset(i, maxY), paint);
+    }
+
+    // Drawing horizontal lines
+    for (double i = 0; i <= maxY; i += step) {
+      canvas.drawLine(Offset(0, i), Offset(maxX, i), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
 
--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -19,11 +19,11 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   void initState() {
     super.initState();
     controller = AnimationController(
-      duration: Duration(seconds: 5),
+      duration: Duration(seconds: 2),
       vsync: this,
     );
-    animation = ColorTween(begin: Colors.white, end: Colors.tealAccent)
-        .animate(controller);
+    animation =
+        ColorTween(begin: Colors.black, end: Colors.white).animate(controller);
     controller.forward();
     controller.addListener(() {
       setState(() {});
@@ -41,66 +41,60 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: animation.value,
-      appBar: AppBar(
-        title: Center(
-          child: Text('Chat App'),
-        ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Flexible(
-                child: Hero(
-                  tag: 'logo',
-                  child: Icon(
-                    Icons.electric_bolt,
-                    size: 60,
-                    color: Colors.orangeAccent.shade400,
+      body: CustomPaint(
+        // Adjust size as needed
+        painter: DrawingBookPainter(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Flexible(
+                  child: Hero(
+                    tag: 'logo',
+                    child: Icon(
+                      Icons.task,
+                      size: 60,
+                      color: Colors.black,
+                    ),
                   ),
                 ),
-              ),
-              DefaultTextStyle(
-                style: const TextStyle(
-                  fontSize: 70.0,
-                  fontWeight: FontWeight.w800,
-                  fontFamily: 'Canterbury',
+                DefaultTextStyle(
+                  style: const TextStyle(
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w800,
+                    fontFamily: 'Canterbury',
+                  ),
+                  child: AnimatedTextKit(
+                    animatedTexts: [
+                      ScaleAnimatedText('Personal'),
+                      ScaleAnimatedText('Task Manager'),
+                    ],
+                    onTap: () {
+                      print("Tap Event");
+                    },
+                  ),
                 ),
-                child: AnimatedTextKit(
-                  animatedTexts: [
-                    ScaleAnimatedText('CHAT'),
-                    ScaleAnimatedText('APP'),
-                  ],
-                  onTap: () {
-                    print("Tap Event");
-                  },
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          roundedButton(
-            buttonColor: Colors.blueAccent.shade200,
-            buttonText: 'LOGIN',
-            buttonDest: () {
-              Navigator.pushNamed(context, LoginScreen.loginScreen);
-            },
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          roundedButton(
-            buttonColor: Colors.cyan,
-            buttonText: 'Register',
-            buttonDest: () {
-              Navigator.pushNamed(context, Registration.registrationScreen);
-            },
-          )
-        ],
+              ],
+            ),
+            roundedButton(
+              buttonColor: Colors.blueAccent.shade200,
+              buttonText: 'LOGIN',
+              buttonDest: () {
+                Navigator.pushNamed(context, LoginScreen.loginScreen);
+              },
+            ),
+            roundedButton(
+              buttonColor: Colors.cyan,
+              buttonText: 'REGISTER',
+              buttonDest: () {
+                Navigator.pushNamed(context, Registration.registrationScreen);
+              },
+            )
+          ],
+        ),
       ),
     );
   }
@@ -114,25 +108,74 @@ class roundedButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      style: ButtonStyle(
-        elevation: MaterialStatePropertyAll(8.0),
-        shadowColor: MaterialStatePropertyAll(Colors.black),
-        backgroundColor: MaterialStatePropertyAll(buttonColor),
-        fixedSize: MaterialStatePropertyAll(
-          Size(300, 50),
-        ),
-        shape: MaterialStatePropertyAll(
-          RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(50),
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(
+            10), // Adjust border radius as needed
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black, // Shadow color
+            offset:
+            Offset(5, 5), // No offset in X and Y direction
+            blurRadius: 0, // Adjust blur radius as needed
+            spreadRadius:
+            0, // Negative spread radius to prevent spreading on the right and bottom sides
           ),
-        ),
+        ],
       ),
-      onPressed: buttonDest,
-      child: Text(
-        buttonText!,
-        style: TextStyle(color: Colors.white, fontSize: 15.0, letterSpacing: 3),
+      child: TextButton(
+        style: ButtonStyle(
+          elevation: MaterialStateProperty.all(8.0),
+          backgroundColor: MaterialStateProperty.all(
+              Colors.lightBlueAccent.shade400), // Set background color to white
+          shape: MaterialStateProperty.all(
+            // Apply shape with border radius
+            RoundedRectangleBorder(
+              borderRadius:
+                  BorderRadius.circular(10), // Adjust border radius as needed
+              side: BorderSide(
+                  width: 1,
+                  color: Colors.black), // Add border with width and color
+            ),
+          ),
+          minimumSize: MaterialStateProperty.all(
+              Size(300, 50),), // Set button size
+        ),
+        onPressed: buttonDest,
+        child: Text(
+          buttonText!,
+          style: TextStyle(color: Colors.white, fontSize: 15.0, letterSpacing: 3),
+        ),
       ),
     );
+  }
+}
+
+class DrawingBookPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.black.withOpacity(0.1)
+      ..strokeWidth = 1.0;
+
+    final double step = 15.0; // Adjust the spacing between lines
+    final double maxX = size.width;
+    final double maxY = size.height;
+
+    // Drawing vertical lines
+    for (double i = 0; i <= maxX; i += step) {
+      canvas.drawLine(Offset(i, 0), Offset(i, maxY), paint);
+    }
+
+    // Drawing horizontal lines
+    for (double i = 0; i <= maxY; i += step) {
+      canvas.drawLine(Offset(0, i), Offset(maxX, i), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
